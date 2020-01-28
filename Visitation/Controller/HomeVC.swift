a082b78ea0c02a1c14f034e661cbc41d811143de
raju_lonka <raju@quadrantsystems.com>
2020-01-28 12:24:13 +0530

    Login api created.

--- Visitation/Controller/HomeVC.swift
+++ Visitation/Controller/HomeVC.swift
@@ -15,8 +15,35 @@ class HomeVC: UIViewController {
 
         // Do any additional setup after loading the view.
     }
+    func login(){
     
+    let url = "http://localhost:8888/connexion/login=\("_login")&pass=\("_password")"
+    let urlString = url.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())!
+    let request = NSURLRequest(URL: NSURL(string: urlString)!)
 
+        let loginTask = URLSession.sharedSession().dataTaskWithRequest(request, completionHandler: { (data, response, error) in
+
+        guard let data = data, let _ = response, error == nil else {
+            print("error")
+            return
+        }
+
+        /*do {
+            let json = try NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments)
+            print(json)
+        } catch let error as NSError {
+            print("json error: \(error.localizedDescription)")
+        }*/
+        if let httpStatus = response as? NSHTTPURLResponse, httpStatus.statusCode != 200 {           // check for http errors
+      print("statusCode should be 200, but is \(httpStatus.statusCode)")
+      print("response = \(response)")
+    }
+
+    let responseString = String(data: data!, encoding:NSUTF8StringEncoding)
+    print("responseString = \(responseString)")
+    })
+    loginTask.resume()
+    }
     /*
     // MARK: - Navigation
 
